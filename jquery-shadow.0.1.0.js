7ca163954a6376107ae96a85af553a3cc4136b13
MeiKatz <appointedaddress@gmail.com>
2013-06-08 23:25:34 +0200

    support for options "applyAuthorStyles" and "resetStyleInheritance"

--- jquery-shadow.0.1.0.js
+++ jquery-shadow.0.1.0.js
@@ -2,16 +2,27 @@
     $.fn.shadow = function ( callback ) {
         var $this = $( this );
         
-        if ( $this.length === 1 && typeof callback === "undefined" ) {
+        if ( $this.length === 1 && typeof callback !== "function" ) {
+            var options = ( typeof callback === "object" ) ? callback : {};
             var el = this[0];
-            
-            if ( el.createShadowRoot ) {
-                return ( el.shadowRoot === null       ? el.createShadowRoot()       : el.shadowRoot );
-            } else if ( el.webkitCreateShadowRoot ) {
-                return ( el.webkitShadowRoot === null ? el.webkitCreateShadowRoot() : el.webkitShadowRoot );
+                        
+            if ( el.createShadowRoot && typeof el.shadowRoot !== "undefined" ) {
+                var shadow = ( el.shadowRoot === null       ? el.createShadowRoot()       : el.shadowRoot );
+            } else if ( el.webkitCreateShadowRoot && typeof el.webkitShadowRoot !== "undefined" ) {
+                var shadow = ( el.webkitShadowRoot === null ? el.webkitCreateShadowRoot() : el.webkitShadowRoot );
             } else {
                 return null;
             }
+            
+            if ( options.hasOwnProperty( "applyAuthorStyles" ) ) {
+                shadow.applyAuthorStyles = !!options.applyAuthorStyles;
+            }
+            
+            if ( options.hasOwnProperty( "resetStyleInheritance" ) ) {
+                shadow.resetStyleInheritance = !!options.resetStyleInheritance;
+            }
+            
+            return shadow;
         }
         
         if ( typeof callback !== "function" ) {
